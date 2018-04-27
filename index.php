aa4422bbb4ad96dfcfd7c65ba7bc630178581e0b
kirigo karanja <sharonkirigo@gmail.com>
2018-04-27 13:40:31 +0300

    this is the first upload of the website

--- index.php
+++ index.php
@@ -5,3 +5,394 @@
  * Date: 22/04/2018
  * Time: 20:34
  */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Pentagon Interior | Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="">
+
+
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+
+    <!-- CSS Files
+    ================================================== -->
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="css/jpreloader.css" type="text/css">
+    <link rel="stylesheet" href="css/animate.css" type="text/css">
+    <link rel="stylesheet" href="css/plugin.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.theme.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.transitions.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/index.css" type="text/css">
+
+
+    <!-- custom background -->
+    <link rel="stylesheet" href="css/bg.css" type="text/css">
+
+    <!-- color scheme -->
+    <link rel="stylesheet" href="css/color.css" type="text/css" id="colors">
+
+    <!-- load fonts -->
+    <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.css" type="text/css">
+    <link rel="stylesheet" href="fonts/elegant_font/HTML_CSS/style.css" type="text/css">
+    <link rel="stylesheet" href="fonts/et-line-font/style.css" type="text/css">
+
+<body id="homepage">
+
+<div id="wrapper">
+
+    <!-- header begin -->
+    <header>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- logo begin -->
+                    <div id="logo">
+                        <a href="index.php">
+                            <img class="logo" src="images/icon.PNG" alt="">
+                        </a>
+                    </div>
+                    <!-- logo close -->
+
+                    <!-- small button begin -->
+                    <span id="menu-btn"></span>
+                    <!-- small button close -->
+
+                    <!-- mainmenu begin -->
+                    <nav>
+                        <ul id="mainmenu">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="#">About Us</a>
+                                <ul>
+                                    <li><a href="#">Our History</a></li>
+                                    <li><a href="#">Our Services</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Clientele</a></li>
+                            <li><a href="#">Gallery</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </nav>
+
+                </div>
+                <!-- mainmenu close -->
+
+            </div>
+        </div>
+    </header>
+    <!-- header close -->
+
+
+
+
+
+    <!-- content begin -->
+    <div id="content" class="no-bottom no-top">
+        <div id="content" class="no-bottom no-top" style="background-image: url('images/wide3.jpg');width: 100%; height: 100vh;">
+
+        </div>
+        <!-- section begin -->
+        <section id="section-about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 text-center wow fadeInUp">
+                        <h1>What We Do</h1>
+                        <div class="separator"><span><i class="fa fa-circle"></i></span></div>
+                        <div class="spacer-single"></div>
+                    </div>
+
+
+                    <div class="col-md-4 wow fadeInLeft">
+                        <h3><span class="id-color">Residential</span> Design</h3>
+                        Modern home layout of the master bedroom layout.
+                        <div class="spacer-single"></div>
+                        <a class="image-popup-no-margins" href="images/bg-6.jpg">
+                            <img src="images/services1.jpg" class="img-responsive" alt="">
+                        </a>
+                    </div>
+
+                    <div class="col-md-4 wow fadeInUp" data-wow-delay=".2s">
+                        <h3><span class="id-color">Office</span> Design</h3>
+                        Office space layout with a modern twist and aired rooms.
+                        <div class="spacer-single"></div>
+                        <img src="images/gallery7.jpg" class="img-responsive" alt="">
+                    </div>
+
+                    <div class="col-md-4 wow fadeInRight">
+                        <h3><span class="id-color">Commercial</span> Design</h3>
+                        shop layout with a showcase of the shop's merchandise.
+                        <div class="spacer-single"></div>
+                        <img src="images/gallery4.jpg" class="img-responsive" alt="">
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!-- section close -->
+
+
+        <!--About-->
+        <section id="content-3-10" class="content-block data-section nopad content-3-10">
+            <div class="image-container col-sm-6 col-xs-12 pull-left">
+                <div class="background-image-holder">
+
+                </div>
+            </div>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6 col-sm-offset-6 col-xs-12 content">
+                        <div class="editContent">
+                            <h1>About Our Company</h1>
+                            <div class="separator"><span><i class="fa fa-circle"></i></span></div>
+                        </div>
+                        <div class="editContent">
+                            <p>Pentagon Interior is interior design with a difference….</p>
+                            <p> We pride ourselves on our creative edge whilst remaining true to balance, form and space. An old-fashioned work ethic, family values, professionalism and integrity that come from 30 years in the construction and interior design industry.</p>
+                        </div>
+                        <a href="#gallery" class="btn btn-outline btn-outline outline-dark">Our Gallery</a>
+                    </div>
+                </div><!-- /.row-->
+            </div><!-- /.container -->
+        </section>
+
+        <!-- section begin -->
+        <section id="section-portfolio" class="no-top no-bottom" aria-label="section-portfolio">
+            <div class="container">
+
+                <div class="spacer-single"></div>
+
+                <!-- portfolio filter begin -->
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 text-center wow fadeInUp">
+                        <h1>Gallery</h1>
+                        <div class="separator"><span><i class="fa fa-circle"></i></span></div>
+                        <div class="spacer-single"></div>
+                    </div>
+                </div>
+                <!-- portfolio filter close -->
+
+            </div>
+
+            <div id="gallery" class="gallery full-gallery de-gallery pf_full_width wow fadeInUp" data-wow-delay=".3s">
+
+                <!-- gallery item -->
+                <div class="item residential">
+                    <div class="picframe">
+                        <a class="simple-ajax-popup-align-top" href="#">
+                                <span class="overlay">
+                                    <span class="pf_text">
+                                        <span class="project-name">Floor Space</span>
+                                    </span>
+                                </span>
+                        </a>
+                        <img src="images/gallery2.jpg" alt="" />
+                    </div>
+                </div>
+                <!-- close gallery item -->
+
+                <!-- gallery item -->
+                <div class="item hospitaly">
+                    <div class="picframe">
+                        <a class="simple-ajax-popup-align-top" href="#">
+                                <span class="overlay">
+                                    <span class="pf_text">
+                                        <span class="project-name">Modern Lighting</span>
+                                    </span>
+                                </span>
+                        </a>
+
+                        <img src="images/gallery1.jpg" alt="" />
+                    </div>
+                </div>
+                <!-- close gallery item -->
+
+                <!-- gallery item -->
+                <div class="item hospitaly">
+                    <div class="picframe">
+                        <a class="simple-ajax-popup-align-top" href="#">
+                                <span class="overlay">
+                                    <span class="pf_text">
+                                        <span class="project-name">Wall Arrangement</span>
+                                    </span>
+                                </span>
+                        </a>
+
+                        <img src="images/gallery3.jpg" alt="" />
+                    </div>
+                </div>
+                <!-- close gallery item -->
+
+                <!-- gallery item -->
+                <div class="item residential">
+                    <div class="picframe">
+                        <a class="simple-ajax-popup-align-top" href="#">
+                                <span class="overlay">
+                                    <span class="pf_text">
+                                        <span class="project-name">Showcase Design</span>
+                                    </span>
+                                </span>
+                        </a>
+                        <img src="images/gallery4.jpg" alt="" />
+                    </div>
+                </div>
+                <!-- close gallery item -->
+
+                <!-- gallery item -->
+                <div class="item office">
+                    <div class="picframe">
+                        <a class="simple-ajax-popup-align-top" href="#">
+                                <span class="overlay">
+                                    <span class="pf_text">
+                                        <span class="project-name">Office Walls</span>
+                                    </span>
+                                </span>
+                        </a>
+                        <img src="images/gallery5.jpg" alt="" />
+                    </div>
+                </div>
+                <!-- close gallery item -->
+
+                <!-- gallery item -->
+                <div class="item commercial">
+                    <div class="picframe">
+                        <a class="simple-ajax-popup-align-top" href="#">
+                                <span class="overlay">
+                                    <span class="pf_text">
+                                        <span class="project-name">Livingroom Arrangement</span>
+                                    </span>
+                                </span>
+                        </a>
+                        <img src="images/gallery6.jpg" alt="" />
+                    </div>
+                </div>
+                <!-- close gallery item -->
+
+                <!-- gallery item -->
+                <div class="item residential">
+                    <div class="picframe">
+                        <a class="simple-ajax-popup-align-top" href="#">
+                                <span class="overlay">
+                                    <span class="pf_text">
+                                        <span class="project-name">Office Layout</span>
+                                    </span>
+                                </span>
+                        </a>
+
+                        <img src="images/gallery7.jpg" alt="" />
+                    </div>
+                </div>
+                <!-- close gallery item -->
+
+                <!-- gallery item -->
+                <div class="item office">
+                    <div class="picframe">
+                        <a class="simple-ajax-popup-align-top" href="#">
+                                <span class="overlay">
+                                    <span class="pf_text">
+                                        <span class="project-name">Showroom Arrangement</span>
+                                    </span>
+                                </span>
+                        </a>
+
+                        <img src="images/gallery8.jpg" alt="" />
+                    </div>
+                </div>
+                <!-- close gallery item -->
+
+            </div>
+
+            <div id="loader-area">
+                <div class="project-load"></div>
+            </div>
+        </section>
+        <!-- section close -->
+
+
+        <!-- section begin -->
+        <section id="view-all-projects" class="call-to-action bg-color text-center" data-speed="5" data-type="background" aria-label="view-all-projects">
+            <a href="#" class="btn btn-line-black btn-big">View All Projects</a>
+        </section>
+        <!-- logo carousel section close -->
+    </div>
+    <!-- footer begin -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div style="width: 100%; height: 300px;" id="map"></div>
+                    <script async defer
+                            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgWjyq3396nSbiVANvEddd38Nt07SW8qU&callback=initMap">
+                    </script>
+                    <br>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="widget widget_recent_post">
+                        <h3>Quick Links</h3>
+                        <ul>
+                            <li><a href="#">Company History</a></li>
+                            <li><a href="#">Our services</a></li>
+                            <li><a href="#">Clientele</a></li>
+                            <li><a href="#">Gallery</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <h3>Contact Us</h3>
+                    <div class="widget widget-address">
+                        <address>
+                            <span><strong>Address:</strong>Road 1, Nationwide Godowns, Godown 7, Baba Dogo Rd, Nairobi</span>
+                            <span><strong>Phone:</strong>+254 737 525209</span>
+                            <span><strong>Email:</strong><a href="mailto:contact@archi-interior.com">contact@pentagoninterior.com</a></span>
+                            <span><strong>Web:</strong><a href="#">http://pentagoninterior.com</a></span>
+                        </address>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <a href="#" id="back-to-top"></a>
+    </footer>
+    <!-- footer close -->
+</div>
+
+
+<!-- Javascript Files
+================================================== -->
+<script src="js/jquery.min.js"></script>
+<script src="js/map.js"></script>
+<script src="js/jpreLoader.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.isotope.min.js"></script>
+<script src="js/easing.js"></script>
+<script src="js/jquery.flexslider-min.js"></script>
+<script src="js/jquery.scrollto.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/jquery.countTo.js"></script>
+<script src="js/classie.js"></script>
+<script src="js/video.resize.js"></script>
+<script src="js/validation.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/enquire.min.js"></script>
+<script src="js/designesia.js"></script>
+
+</body>
+
+</html>
