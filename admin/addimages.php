8e6c248c5f7372498a39471bad1402d25db120a9
kirigo karanja <sharonkirigo@gmail.com>
2018-05-18 19:40:25 +0300

    sweet alert additions

--- admin/addimages.php
+++ admin/addimages.php
@@ -1,3 +1,11 @@
+<html>
+<head>
+    <script src="../dist/sweetalert.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../dist/sweetalert.css">
+    <style>body{background-image: url("../images/wide3.jpg")}</style>
+
+</head>
+<body>
 <?php
 /**
  * Created by PhpStorm.
@@ -20,6 +28,36 @@ $date = date('Y-m-d');
 $sql3 = "INSERT INTO `clientimage`(`image_name`, `client_id`, `date`) VALUES ('$images', '$userid', '$date')";
 $result3 = $conn->query($sql3);
 
+if ($result3){
+  ?>
+    <script>
+        swal({
+            title: "Success",
+            text: "Upload successful!",
+            type: "success",
+            timer: 1500,
+            showConfirmButton: false
+        });
+        setTimeout(function () {
+            location.href = "client.php"
+        }, 1000);
+    </script>
+    <?php
+}else{
+    ?>
+    <script>
+        swal({
+            title: "Error",
+            text: "An error ocurred!",
+            type: "error",
+            timer: 1500,
+            showConfirmButton: false
+        });
+        setTimeout(function () {
+            location.href = "image.php"
+        }, 1000);
+    </script>
+    <?php
+}
 
 
-?>
