a99ae4bddad9a78ba0a6dae07fd01b951f6b26ea
kirigo karanja <sharonkirigo@gmail.com>
2018-05-25 10:52:12 +0300

    porfolio additions

--- portfolio.php
+++ portfolio.php
@@ -2,6 +2,338 @@
 /**
  * Created by PhpStorm.
  * User: kirigo karanja
- * Date: 14/05/2018
- * Time: 11:32
+ * Date: 22/04/2018
+ * Time: 20:34
  */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Pentagon Interior | Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="">
+    <style media="screen">
+      #cust-menu ul{
+      text-decoration: none;
+      }
+      #cust-menu li{
+        display: inline;
+
+        padding: 3px;
+      }
+      #cust-menu li a{
+        color: #ccc;
+        font-size: 18px;
+        text-transform: uppercase;
+      }
+        .porfolio{
+            text-align: center;
+            padding-top: 40px;
+            font-size: 20px;
+            line-height: 1.5 em;
+        }
+    </style>
+
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+
+    <!-- CSS Files
+    ================================================== -->
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="css/jpreloader.css" type="text/css">
+    <link rel="stylesheet" href="css/animate.css" type="text/css">
+    <link rel="stylesheet" href="css/plugin.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.theme.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.transitions.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/index.css" type="text/css">
+    <link rel="stylesheet" href="css/new.css" type="text/css">
+
+
+    <!-- custom background -->
+    <link rel="stylesheet" href="css/bg.css" type="text/css">
+
+    <!-- color scheme -->
+    <link rel="stylesheet" href="css/color.css" type="text/css" id="colors">
+
+    <!-- load fonts -->
+    <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.css" type="text/css">
+    <link rel="stylesheet" href="fonts/elegant_font/HTML_CSS/style.css" type="text/css">
+    <link rel="stylesheet" href="fonts/et-line-font/style.css" type="text/css">
+
+<body id="homepage">
+
+<div id="wrapper">
+
+    <!-- header begin -->
+    <header>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- logo begin -->
+                    <div id="logo">
+                        <a href="index.php">
+                            <img class="logo" src="images/icon.PNG" alt="">
+                        </a>
+                    </div>
+                    <!-- logo close -->
+
+                    <!-- small button begin -->
+                    <span id="menu-btn"></span>
+                    <!-- small button close -->
+
+                    <!-- mainmenu begin -->
+                    <nav>
+                        <ul id="mainmenu">
+                            <li><a href="index.php" >Home</a></li>
+                            <li><a href="about.php">About Us</a>
+                                <ul>
+                                    <li><a href="history.php">Our History</a></li>
+                                    <li><a href="services.php">Our Services</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="clientele.php">Clientele</a></li>
+                            <li><a href="gallery.php">Gallery</a></li>
+                            <li><a href="portfolio.php" class="active">Portfolio</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </nav>
+
+                </div>
+                <!-- mainmenu close -->
+
+            </div>
+        </div>
+    </header>
+    <!-- header close -->
+        <!-- content begin -->
+<!--    <div id="content" class="no-bottom no-top">-->
+<!--        <div id="content" class="no-bottom no-top" style="background-image: url('images/wide3.jpg');width: 100%; height: 100vh;">-->
+<!--            <div id="homepagecontent">-->
+<!--                <br><br><br><br><br><br>-->
+<!--                <h3>our expertise for</h3>-->
+<!--                <h2>interior design</h2><br>-->
+<!--                <a href="#">Our Portfolio</a>-->
+<!--            </div><br><br>-->
+<!--            <a href="#section-about" id="homepageimga"><img src="images/ui.totop.png" id="homepagecontentimg"></a>-->
+<!---->
+<!--        </div>-->
+        <!-- section begin -->
+    <br>
+        <section id="section-about" class="section-about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 text-center wow fadeInUp">
+                        <h1>Our Portfolio in design</h1>
+
+                    <div class="separator"><span><i class="fa fa-circle"></i></span></div>
+                    <div class="row">
+                      <div class="col-md-12">
+                        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                            <div class="navbar-nav">
+                            <ul id="cust-menu">
+                                <li><a class="nav-item nav-link e" href="#core-values">Core Values</a></li>
+                                <li><a class="nav-item nav-link" href="#vision-mission">Vision and Mission</a></li>
+                                <li><a class="nav-item nav-link" href="#Partitions">Smart Partitions</a></li>
+                                <li><a class="nav-item nav-link disabled" href="#ceilings">Smart Ceilings</a></li>
+                                <li><a class="nav-item nav-link disabled" href="#exhibition">Smart Exhibition Stands</a></li>
+                          </ul>
+                            </div>
+                              </div>
+                            </div>
+
+                         </nav>
+                        <div class="spacer-single"></div>
+                    </div>
+                    <!-- Links -->
+
+
+                    <div class="col-md-12 wow fadeInLeft margin" id="core-values" style="padding-bottom: 50px;padding-top: 30px;">
+                        <h3><span class="id-color">Core</span> Values</h3>
+                      <div class="row">
+                          <div class="col-md-6">
+                          <p class="porfolio">Courage means we are entrepreneurial and thus take risks, reach beyond boundaries and experiment</p>
+                          </div>
+
+                          <div class="col-md-6">
+                            <a class="image-popup-no-margins" href="images/bg-6.jpg">
+                                <img src="images/gallery5.jpg" class="img-responsive" alt="">
+                            </a>
+                          </div>
+                      </div>
+                    </div>
+
+                    <div class="col-md-12 wow fadeInLeft margin" id="vision-mission" style="padding-bottom: 50px;padding-top: 30px;">
+                        <h3><span class="id-color">Vision </span>and Mission</h3>
+                      <div class="row">
+                          <div class="col-md-6">
+                          <p class="porfolio">Courage means we are entrepreneurial and thus take risks, reach beyond boundaries and experiment</p>
+                          </div>
+
+                          <div class="col-md-6">
+                            <a class="image-popup-no-margins" href="images/bg-6.jpg">
+                                <img src="images/gallery2.jpg" class="img-responsive" alt="">
+                            </a>
+                          </div>
+                      </div>
+                    </div>
+
+                    <div class="col-md-12 wow fadeInLeft margin" id="Partitions" style="padding-bottom: 50px;padding-top: 30px;">
+                        <h3><span class="id-color">Smart</span> Partitions</h3>
+                      <div class="row">
+                          <div class="col-md-6">
+                          <p class="porfolio">Courage means we are entrepreneurial and thus take risks, reach beyond boundaries and experiment</p>
+                          </div>
+
+                          <div class="col-md-6">
+                            <a class="image-popup-no-margins" href="images/bg-6.jpg">
+                                <img src="images/gallery8.jpg" class="img-responsive" alt="">
+                            </a>
+                          </div>
+                      </div>
+
+                      <div class="col-md-12 wow fadeInLeft margin" id="ceilings" style="padding-bottom: 50px;padding-top: 30px;">
+                          <h3><span class="id-color">Smart</span> Ceilings</h3>
+                        <div class="row">
+                            <div class="col-md-6">
+                            <p class="porfolio">Courage means we are entrepreneurial and thus take risks, reach beyond boundaries and experiment</p>
+                            </div>
+
+                            <div class="col-md-6">
+                              <a class="image-popup-no-margins" href="images/bg-6.jpg">
+                                  <img src="images/gallery6.jpg" class="img-responsive" alt="">
+                              </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12 wow fadeInLeft margin" id="exhibition" style="padding-bottom: 50px;padding-top: 30px;">
+                        <h3><span class="id-color">Exhibition</span>Stands</h3>
+                      <div class="row">
+                          <div class="col-md-6">
+                          <p class="porfolio">Courage means we are entrepreneurial and thus take risks, reach beyond boundaries and experiment</p>
+                          </div>
+
+                          <div class="col-md-6">
+                            <a class="image-popup-no-margins" href="images/bg-6.jpg">
+                                <img src="images/gallery7.jpg" class="img-responsive" alt="">
+                            </a>
+                          </div>
+                      </div>
+                    </div>
+        </section>
+        <!-- section close -->
+
+
+        <!--About-->
+        <section id="content-3-10" class="content-block data-section nopad content-3-10">
+            <div class="image-container col-sm-6 col-xs-12 pull-left">
+                <div class="background-image-holder">
+
+                </div>
+            </div>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6 col-sm-offset-6 col-xs-12 content">
+                        <div class="editContent">
+                            <h1>About Our Company</h1>
+                            <div class="separator"><span><i class="fa fa-circle"></i></span></div>
+                        </div>
+                        <div class="editContent">
+                            <p>Pentagon Interior is interior design with a difference….</p>
+                            <p> We pride ourselves on our creative edge whilst remaining true to balance, form and space. An old-fashioned work ethic, family values, professionalism and integrity that come from 30 years in the construction and interior design industry.</p>
+                        </div>
+                        <a href="#gallery" class="btn btn-outline btn-outline outline-dark">Our Gallery</a>
+                    </div>
+                </div><!-- /.row-->
+            </div><!-- /.container -->
+        </section>
+
+
+
+
+
+        <!-- section begin -->
+        <section id="view-all-projects" class="call-to-action bg-color text-center" data-speed="5" data-type="background" aria-label="view-all-projects">
+            <a href="#" class="btn btn-line-black btn-big">View All Projects</a>
+        </section>
+        <!-- logo carousel section close -->
+    </div>
+    <!-- footer begin -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div style="width: 100%; height: 300px;" id="map"></div>
+                    <script async defer
+                            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgWjyq3396nSbiVANvEddd38Nt07SW8qU&callback=initMap">
+                    </script>
+                    <br>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="widget widget_recent_post">
+                        <h3>Quick Links</h3>
+                        <ul>
+                            <li><a href="history.php">Company History</a></li>
+                            <li><a href="services.php">Our services</a></li>
+                            <li><a href="#">Clientele</a></li>
+                            <li><a href="#">Gallery</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <h3>Contact Us</h3>
+                    <div class="widget widget-address">
+                        <address>
+                            <span><strong>Address:</strong>Road 1, Nationwide Godowns, Godown 7, Baba Dogo Rd, Nairobi</span>
+                            <span><strong>Phone:</strong>+254 737 525209</span>
+                            <span><strong>Email:</strong><a href="mailto:contact@archi-interior.com">contact@pentagoninterior.com</a></span>
+                            <span><strong>Web:</strong><a href="#">http://pentagoninterior.com</a></span>
+                        </address>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <a href="#" id="back-to-top"></a>
+    </footer>
+    <!-- footer close -->
+</div>
+
+
+<!-- Javascript Files
+================================================== -->
+<script src="js/jquery.min.js"></script>
+<script src="js/map.js"></script>
+<script src="js/jpreLoader.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.isotope.min.js"></script>
+<script src="js/easing.js"></script>
+<script src="js/jquery.flexslider-min.js"></script>
+<script src="js/jquery.scrollto.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/jquery.countTo.js"></script>
+<script src="js/classie.js"></script>
+<script src="js/video.resize.js"></script>
+<script src="js/validation.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/enquire.min.js"></script>
+<script src="js/designesia.js"></script>
+
+</body>
+
+</html>
